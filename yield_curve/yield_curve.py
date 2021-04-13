af9307b27929c2b6db579e536f3c8049e3fa5ff4
SayYoungMan <nickte89@gmail.com>
2021-04-13 10:31:54 +0100

    feat: UK yield curve added
    can plot daily curve, 1 week ago and 1month ago. This feature will be added to us yield curve as well

--- yield_curve/yield_curve.py
+++ yield_curve/yield_curve.py
@@ -2,6 +2,9 @@ import urllib.request
 import xml.etree.ElementTree as ET
 from matplotlib import pyplot as plt
 import seaborn as sns
+import requests
+import lxml.html as lh
+import pandas as pd
 
 class yieldCurveGenerator:
     def __init__(self):
@@ -15,6 +18,11 @@ class yieldCurveGenerator:
             'BC_1YEAR', 'BC_2YEAR', 'BC_3YEAR', 'BC_5YEAR', 'BC_7YEAR', 'BC_10YEAR', 'BC_20YEAR', 'BC_30YEAR'
             ]
         self.us_interval_quant = [1/12, 2/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30]
+
+        # UK Yield Curve Related Parameters
+        self.uk_url = 'https://markets.ft.com/data/bonds'
+        self.uk_interval_quant = [1/12, 3/12, 6/12, 1, 2, 3, 4, 5, 7, 8, 9, 10, 15, 20, 30]
+
         
     
     def get_us_data(self):
@@ -31,7 +39,45 @@ class yieldCurveGenerator:
         plt.title('US Daily Treasury Yield Curve Rates')
         plt.show()
 
+    def get_uk_data(self):
+        response = requests.get(self.uk_url)
+        doc = lh.fromstring(response.content)
+        tr_elems = doc.xpath('//tr')
+
+        col = []
+        i = 0
+
+        for t in tr_elems[0]:
+            i += 1
+            name = t.text_content()
+            col.append((name, []))
+
+        for j in range(1, 16):
+            T = tr_elems[j]
+            if len(T) != 5:
+                raise ValueError("Not valid length of row.")
+            i = 0
+            for t in T.iterchildren():
+                data = t.text_content()
+                if i in [1, 3, 4]:
+                    data = float(data.rstrip('%'))
+                col[i][1].append(data)
+                i += 1
+            
+        df = {title:column for (title, column) in col}
+        df = pd.DataFrame(df)
+
+        plt.plot(self.uk_interval_quant, df['Yield'], 'x-')
+        plt.plot(self.uk_interval_quant, df['1 week ago'], 'x-')
+        plt.plot(self.uk_interval_quant, df['1 month ago'], 'x-')
+        plt.xlabel('Time from Today / Years')
+        plt.ylabel('Yield Curve Rate / %')
+        plt.title('UK Daily Yield Curve Rates')
+        plt.legend(['Today', 'Week Ago', 'Month Ago'])
+        plt.show()
+
 
 if __name__ == '__main__':
     ycg = yieldCurveGenerator()
     ycg.get_us_data()
+    ycg.get_uk_data()
